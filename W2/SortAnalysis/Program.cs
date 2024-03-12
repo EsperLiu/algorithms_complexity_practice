1e9639f4183a9082070f6b94cff4f677889090aa
Tao Liu <n9912517@qut.edu.au>
2024-03-12 22:50:15 +1000

    added W2

---
+++ W2/SortAnalysis/Program.cs
@@ -0,0 +1,66 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+SortAnalysisV1(GenerateRandomArray(25000));
+
+/*
+for (int i = 1000; i <= 20000; i = i + 1000)
+{
+    Console.WriteLine("Input size: " + i);
+    for (int j = 0; j < 3; j++)
+    {
+        SortAnalysisV1(GenerateRandomArray(i));
+    }
+}*/
+
+
+
+
+
+
+static int SortAnalysisV1(int[] A)
+{
+    // print out A to see the input
+    //PrintArray(A);
+
+    int count = 0;
+    int n = A.Length;
+    for (int i = 1; i <= n - 1; i++)
+    {
+        int v = A[i];
+        int j = i - 1;
+        while (j >= 0 && ++count> 0 && A[j] > v)
+        {
+        
+            A[j + 1] = A[j];
+            j = j - 1;
+        }
+        A[j + 1] = v;
+    }
+    // Print out A again to see what happened
+    //PrintArray(A);
+    Console.WriteLine("Comparison Count: " +  count);
+    return count;
+}
+
+static void PrintArray(int[] A)
+{
+    for (int i = 0; i < A.Length; i++)
+    {
+        Console.Write(A[i] + " ");
+    }
+    Console.WriteLine();
+}
+
+static int[] GenerateRandomArray(int size)
+{
+    int[] A = new int[size];
+
+    int seed = (int) DateTime.UtcNow.Ticks;
+    Random rnd = new Random(seed);
+
+    for (int i = 0; i < A.Length; i++)
+    {
+        A[i] = rnd.Next(Int32.MaxValue); // (-2,147,483,648 to +2,147,483,647)
+    }
+    return A;
+}
